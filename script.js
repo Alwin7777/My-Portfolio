76a8ec815b239316409b8f223d9a5d584dff16cf
Alwin7777 <49651815+Alwin7777@users.noreply.github.com>
2024-12-06 05:49:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -24,14 +24,14 @@ $(document).ready(function () {
 
   // typing animation script
   var typed = new Typed(".typing", {
-    strings: ["Graphic Designer", "Freelancer."],
+    strings: ["Graphic Designer","Web Designer", Web Developer","Freelancer."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Graphic Designer", "Freelancer."],
+    strings: ["Graphic Designer","Web Designer", Web Developer","Freelancer."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
